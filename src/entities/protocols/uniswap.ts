7055b381cae13d9c0a66ee23a868ee66f024a01b
0xshoegazer <0xshoegazer@gmail.com>
2024-10-01 15:39:39 -0400

    v2 setup

--- src/entities/protocols/uniswap.ts
+++ src/entities/protocols/uniswap.ts
@@ -18,14 +18,24 @@ import { Permit2Permit } from '../../utils/inputTokens'
 import { Currency, TradeType, CurrencyAmount, Percent } from '@baseswapfi/sdk-core'
 import { Trade as V3Trade, Pool, encodeRouteToPath } from '@baseswapfi/v3-sdk2'
 import { Command, RouterTradeType, TradeConfig } from '../Command'
-import { SENDER_AS_RECIPIENT, ROUTER_AS_RECIPIENT, CONTRACT_BALANCE } from '../../utils/constants'
+import { SENDER_AS_RECIPIENT, ROUTER_AS_RECIPIENT, CONTRACT_BALANCE, ETH_ADDRESS } from '../../utils/constants'
 import { encodeFeeBips } from '../../utils/numbers'
-import { BigNumber } from 'ethers'
+import { BigNumber, BigNumberish } from 'ethers'
+
+export type FlatFeeOptions = {
+  amount: BigNumberish
+  recipient: string
+}
 
 // the existing router permit object doesn't include enough data for permit2
 // so we extend swap options with the permit2 permit
+// when safe mode is enabled, the SDK will add an extra ETH sweep for security
+// when useRouterBalance is enabled the SDK will use the balance in the router for the swap
 export type SwapOptions = Omit<RouterSwapOptions, 'inputTokenPermit'> & {
+  useRouterBalance?: boolean
   inputTokenPermit?: Permit2Permit
+  flatFee?: FlatFeeOptions
+  safeMode?: boolean
 }
 
 const REFUND_ETH_PRICE_IMPACT_THRESHOLD = new Percent(50, 100)
@@ -40,20 +50,48 @@ interface Swap<TInput extends Currency, TOutput extends Currency> {
 // also translates trade objects from previous (v2, v3) SDKs
 export class UniswapTrade implements Command {
   readonly tradeType: RouterTradeType = RouterTradeType.UniswapTrade
-  constructor(public trade: RouterTrade<Currency, Currency, TradeType>, public options: SwapOptions) {}
+  readonly payerIsUser: boolean
+
+  constructor(public trade: RouterTrade<Currency, Currency, TradeType>, public options: SwapOptions) {
+    if (!!options.fee && !!options.flatFee) throw new Error('Only one fee option permitted')
+    if (this.inputRequiresWrap) this.payerIsUser = false
+    else if (this.options.useRouterBalance) this.payerIsUser = false
+    else this.payerIsUser = true
+  }
+
+  get inputRequiresWrap(): boolean {
+    // if (!this.isAllV4) {
+    //   return this.trade.inputAmount.currency.isNative
+    // } else {
+    //   // We only support wrapping all ETH or no ETH currently. We cannot support splitting where half needs to be wrapped
+    //   // If the input currency is ETH and the input of the first path is not ETH it must be WETH that needs wrapping
+    //   return this.trade.inputAmount.currency.isNative && !this.trade.swaps[0].route.input.isNative
+    // }
+    // We only support wrapping all ETH or no ETH currently. We cannot support splitting where half needs to be wrapped
+    // If the input currency is ETH and the input of the first path is not ETH it must be WETH that needs wrapping
+    return this.trade.inputAmount.currency.isNative && !this.trade.swaps[0].route.input.isNative
+  }
+
+  get outputRequiresUnwrap(): boolean {
+    // if (!this.isAllV4) {
+    //   return this.trade.outputAmount.currency.isNative
+    // } else {
+    //   // If the output currency is ETH and the output of the swap is not ETH it must be WETH that needs unwrapping
+    //   return this.trade.outputAmount.currency.isNative && !this.trade.swaps[0].route.output.isNative
+    // }
+
+    // If the output currency is ETH and the output of the swap is not ETH it must be WETH that needs unwrapping
+    return this.trade.outputAmount.currency.isNative && !this.trade.swaps[0].route.output.isNative
+  }
 
   encode(planner: RoutePlanner, _config: TradeConfig): void {
-    let payerIsUser = true
-
     // If the input currency is the native currency, we need to wrap it with the router as the recipient
-    if (this.trade.inputAmount.currency.isNative) {
+    if (this.inputRequiresWrap) {
       // TODO: optimize if only one v2 pool we can directly send this to the pool
       planner.addCommand(CommandType.WRAP_ETH, [
         ROUTER_AS_RECIPIENT,
         this.trade.maximumAmountIn(this.options.slippageTolerance).quotient.toString(),
       ])
-      // since WETH is now owned by the router, the router pays for inputs
-      payerIsUser = false
     }
     // The overall recipient at the end of the trade, SENDER_AS_RECIPIENT uses the msg.sender
     this.options.recipient = this.options.recipient ?? SENDER_AS_RECIPIENT
@@ -64,20 +102,18 @@ export class UniswapTrade implements Command {
     //      in that the reversion probability is lower
     const performAggregatedSlippageCheck =
       this.trade.tradeType === TradeType.EXACT_INPUT && this.trade.routes.length > 2
-    const outputIsNative = this.trade.outputAmount.currency.isNative
-    const inputIsNative = this.trade.inputAmount.currency.isNative
-    const routerMustCustody = performAggregatedSlippageCheck || outputIsNative || !!this.options.fee
+    const routerMustCustody = performAggregatedSlippageCheck || this.outputRequiresUnwrap || hasFeeOption(this.options)
 
     for (const swap of this.trade.swaps) {
       switch (swap.route.protocol) {
         case Protocol.V2:
-          addV2Swap(planner, swap, this.trade.tradeType, this.options, payerIsUser, routerMustCustody)
+          addV2Swap(planner, swap, this.trade.tradeType, this.options, this.payerIsUser, routerMustCustody)
           break
         case Protocol.V3:
-          addV3Swap(planner, swap, this.trade.tradeType, this.options, payerIsUser, routerMustCustody)
+          addV3Swap(planner, swap, this.trade.tradeType, this.options, this.payerIsUser, routerMustCustody)
           break
         case Protocol.MIXED:
-          addMixedSwap(planner, swap, this.trade.tradeType, this.options, payerIsUser, routerMustCustody)
+          addMixedSwap(planner, swap, this.trade.tradeType, this.options, this.payerIsUser, routerMustCustody)
           break
         default:
           throw new Error('UNSUPPORTED_TRADE_PROTOCOL')
@@ -107,9 +143,28 @@ export class UniswapTrade implements Command {
         }
       }
 
+      // If there is a flat fee, that absolute amount is sent to the fee recipient
+      // In the case where ETH is the output currency, the fee is taken in WETH (for gas reasons)
+      if (!!this.options.flatFee) {
+        const feeAmount = this.options.flatFee.amount
+        if (minimumAmountOut.lt(feeAmount)) throw new Error('Flat fee amount greater than minimumAmountOut')
+
+        planner.addCommand(CommandType.TRANSFER, [
+          this.trade.outputAmount.currency.wrapped.address,
+          this.options.flatFee.recipient,
+          feeAmount,
+        ])
+
+        // If the trade is exact output, and a fee was taken, we must adjust the amount out to be the amount after the fee
+        // Otherwise we continue as expected with the trade's normal expected output
+        if (this.trade.tradeType === TradeType.EXACT_OUTPUT) {
+          minimumAmountOut = minimumAmountOut.sub(feeAmount)
+        }
+      }
+
       // The remaining tokens that need to be sent to the user after the fee is taken will be caught
       // by this if-else clause.
-      if (outputIsNative) {
+      if (this.outputRequiresUnwrap) {
         planner.addCommand(CommandType.UNWRAP_WETH, [this.options.recipient, minimumAmountOut])
       } else {
         planner.addCommand(CommandType.SWEEP, [
@@ -120,11 +175,13 @@ export class UniswapTrade implements Command {
       }
     }
 
-    if (inputIsNative && (this.trade.tradeType === TradeType.EXACT_OUTPUT || riskOfPartialFill(this.trade))) {
+    if (this.inputRequiresWrap && (this.trade.tradeType === TradeType.EXACT_OUTPUT || riskOfPartialFill(this.trade))) {
       // for exactOutput swaps that take native currency as input
       // we need to send back the change to the user
       planner.addCommand(CommandType.UNWRAP_WETH, [this.options.recipient, 0])
     }
+
+    if (this.options.safeMode) planner.addCommand(CommandType.SWEEP, [ETH_ADDRESS, this.options.recipient, 0])
   }
 }
 
@@ -288,4 +345,8 @@ function addMixedSwap<TInput extends Currency, TOutput extends Currency>(
 // if price impact is very high, there's a chance of hitting max/min prices resulting in a partial fill of the swap
 function riskOfPartialFill(trade: RouterTrade<Currency, Currency, TradeType>): boolean {
   return trade.priceImpact.greaterThan(REFUND_ETH_PRICE_IMPACT_THRESHOLD)
+}
+
+function hasFeeOption(swapOptions: SwapOptions): boolean {
+  return !!swapOptions.fee || !!swapOptions.flatFee
 }
--- src/swapRouter.ts
+++ src/swapRouter.ts
@@ -5,135 +5,42 @@ import { BigNumber, BigNumberish } from 'ethers'
 import { Trade as RouterTrade } from '@baseswapfi/router-sdk'
 import { Currency, TradeType } from '@baseswapfi/sdk-core'
 import { MethodParameters } from '@baseswapfi/v3-sdk2'
-import { Command, RouterTradeType } from './entities/Command'
-import { Market, NFTTrade, SupportedProtocolsData } from './entities/NFTTrade'
 import { UniswapTrade, SwapOptions } from './entities/protocols/uniswap'
-import { UnwrapWETH } from './entities/protocols/unwrapWETH'
-import { CommandType, RoutePlanner } from './utils/routerCommands'
+import { RoutePlanner } from './utils/routerCommands'
 import { encodePermit } from './utils/inputTokens'
-import { ROUTER_AS_RECIPIENT, SENDER_AS_RECIPIENT, ETH_ADDRESS } from './utils/constants'
-import { SeaportTrade } from './entities'
 
 export type SwapRouterConfig = {
   sender?: string // address
   deadline?: BigNumberish
 }
 
-type SupportedNFTTrade = NFTTrade<SupportedProtocolsData>
-
 export abstract class SwapRouter {
   public static INTERFACE: Interface = new Interface(RouterABI)
 
-  public static swapCallParameters(trades: Command[] | Command, config: SwapRouterConfig = {}): MethodParameters {
-    if (!Array.isArray(trades)) trades = [trades]
-
-    const nftTrades = trades.filter((trade, _, []) => trade.hasOwnProperty('market')) as SupportedNFTTrade[]
-    const allowRevert = nftTrades.length == 1 && nftTrades[0].orders.length == 1 ? false : true
+  public static swapCallParameters(
+    trades: RouterTrade<Currency, Currency, TradeType>,
+    options: SwapOptions
+  ): MethodParameters {
+    // TODO: use permit if signature included in swapOptions
     const planner = new RoutePlanner()
 
-    // track value flow to require the right amount of native value
-    let currentNativeValueInRouter = BigNumber.from(0)
-    let transactionValue = BigNumber.from(0)
+    const trade: UniswapTrade = new UniswapTrade(trades, options)
 
-    // tracks the input tokens (and ETH) used to buy NFTs to allow us to sweep
-    let nftInputTokens = new Set<string>()
+    const inputCurrency = trade.trade.inputAmount.currency
+    invariant(!(inputCurrency.isNative && !!options.inputTokenPermit), 'NATIVE_INPUT_PERMIT')
 
-    for (const trade of trades) {
-      /**
-       * is NFTTrade
-       */
-      if (trade.tradeType == RouterTradeType.NFTTrade) {
-        const nftTrade = trade as SupportedNFTTrade
-        nftTrade.encode(planner, { allowRevert })
-        const tradePrice = nftTrade.getTotalPrice()
-
-        if (nftTrade.market == Market.Seaport) {
-          const seaportTrade = nftTrade as SeaportTrade
-          const seaportInputTokens = seaportTrade.getInputTokens()
-          seaportInputTokens.forEach((inputToken) => {
-            nftInputTokens.add(inputToken)
-          })
-        } else {
-          nftInputTokens.add(ETH_ADDRESS)
-        }
-
-        // send enough native value to contract for NFT purchase
-        if (currentNativeValueInRouter.lt(tradePrice)) {
-          transactionValue = transactionValue.add(tradePrice.sub(currentNativeValueInRouter))
-          currentNativeValueInRouter = BigNumber.from(0)
-        } else {
-          currentNativeValueInRouter = currentNativeValueInRouter.sub(tradePrice)
-        }
-        /**
-         * is UniswapTrade
-         */
-      } else if (trade.tradeType == RouterTradeType.UniswapTrade) {
-        const uniswapTrade = trade as UniswapTrade
-        const inputIsNative = uniswapTrade.trade.inputAmount.currency.isNative
-        const outputIsNative = uniswapTrade.trade.outputAmount.currency.isNative
-        const swapOptions = uniswapTrade.options
-
-        invariant(!(inputIsNative && !!swapOptions.inputTokenPermit), 'NATIVE_INPUT_PERMIT')
-
-        if (!!swapOptions.inputTokenPermit) {
-          encodePermit(planner, swapOptions.inputTokenPermit)
-        }
-
-        if (inputIsNative) {
-          transactionValue = transactionValue.add(
-            BigNumber.from(uniswapTrade.trade.maximumAmountIn(swapOptions.slippageTolerance).quotient.toString())
-          )
-        }
-        // track amount of native currency in the router
-        if (outputIsNative && swapOptions.recipient == ROUTER_AS_RECIPIENT) {
-          currentNativeValueInRouter = currentNativeValueInRouter.add(
-            BigNumber.from(uniswapTrade.trade.minimumAmountOut(swapOptions.slippageTolerance).quotient.toString())
-          )
-        }
-        uniswapTrade.encode(planner, { allowRevert: false })
-        /**
-         * is UnwrapWETH
-         */
-      } else if (trade.tradeType == RouterTradeType.UnwrapWETH) {
-        const UnwrapWETH = trade as UnwrapWETH
-        trade.encode(planner, { allowRevert: false })
-        currentNativeValueInRouter = currentNativeValueInRouter.add(UnwrapWETH.amount)
-        /**
-         * else
-         */
-      } else {
-        throw 'trade must be of instance: UniswapTrade or NFTTrade'
-      }
+    if (options.inputTokenPermit) {
+      encodePermit(planner, options.inputTokenPermit)
     }
 
-    // TODO: matches current logic for now, but should eventually only sweep for multiple NFT trades
-    // or NFT trades with potential slippage (i.e. sudo).
-    // Note: NFTXV2 sends excess ETH to the caller (router), not the specified recipient
-    nftInputTokens.forEach((inputToken) => {
-      planner.addCommand(CommandType.SWEEP, [inputToken, SENDER_AS_RECIPIENT, 0])
+    const nativeCurrencyValue = inputCurrency.isNative
+      ? BigNumber.from(trade.trade.maximumAmountIn(options.slippageTolerance).quotient.toString())
+      : BigNumber.from(0)
+
+    trade.encode(planner, { allowRevert: false })
+    return SwapRouter.encodePlan(planner, nativeCurrencyValue, {
+      deadline: options.deadlineOrPreviousBlockhash ? BigNumber.from(options.deadlineOrPreviousBlockhash) : undefined,
     })
-    return SwapRouter.encodePlan(planner, transactionValue, config)
-  }
-
-  /**
-   * @deprecated in favor of swapCallParameters. Update before next major version 2.0.0
-   * This version does not work correctly for Seaport ERC20->NFT purchases
-   * Produces the on-chain method name to call and the hex encoded parameters to pass as arguments for a given swap.
-   * @param trades to produce call parameters for
-   */
-  public static swapNFTCallParameters(trades: SupportedNFTTrade[], config: SwapRouterConfig = {}): MethodParameters {
-    let planner = new RoutePlanner()
-    let totalPrice = BigNumber.from(0)
-
-    const allowRevert = trades.length == 1 && trades[0].orders.length == 1 ? false : true
-
-    for (const trade of trades) {
-      trade.encode(planner, { allowRevert })
-      totalPrice = totalPrice.add(trade.getTotalPrice())
-    }
-
-    planner.addCommand(CommandType.SWEEP, [ETH_ADDRESS, SENDER_AS_RECIPIENT, 0])
-    return SwapRouter.encodePlan(planner, totalPrice, config)
   }
 
   /**
